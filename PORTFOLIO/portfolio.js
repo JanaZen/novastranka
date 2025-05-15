6ec6b73e54b089ecfbc980faa6f11760f1b457a2
JanaZen <jana.raudenska@email.cz>
2025-05-15 15:34:29 +0200

    Add files via upload

--- PORTFOLIO/portfolio.js
+++ PORTFOLIO/portfolio.js
@@ -1,20 +1,24 @@
 const carousels = [];
 
+
 document.querySelectorAll('.carousel-container').forEach((container, index) => {
   const slides = container.querySelector('.slides');
   const prevBtn = container.querySelector('.prev');
   const nextBtn = container.querySelector('.next');
   const totalSlides = container.querySelectorAll('.slide').length;
-  let currentIndex = 0;
+  let currentIndex = 3;
+
 
   const updateSlide = () => {
     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
   };
 
+
   prevBtn.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
     updateSlide();
   });
+
 
   nextBtn.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % totalSlides;
